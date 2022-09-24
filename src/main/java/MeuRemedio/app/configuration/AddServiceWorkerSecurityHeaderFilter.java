d52467d891bb1c9d768a349474e677c785ee68d1
EricJSCabral <eric.jscabral@gmail.com>
2022-09-24 18:04:22 -0300

    teste scope

---
+++ src/main/java/MeuRemedio/app/configuration/AddServiceWorkerSecurityHeaderFilter.java
@@ -0,0 +1,28 @@
+package MeuRemedio.app.configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/script/sw.js")
+public class AddServiceWorkerSecurityHeaderFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Service-Worker-Allowed", "/");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+}
--- src/main/java/MeuRemedio/app/configuration/DataConfiguration.java
+++ src/main/java/MeuRemedio/app/configuration/DataConfiguration.java
@@ -16,19 +16,19 @@ public class DataConfiguration {
 
 
                   //Metodo para base de dados locais
-//    @Bean
-//    public DataSource datasource() {
-//        try {
-//            DriverManagerDataSource driver = new DriverManagerDataSource();
-//            driver.setDriverClassName("org.postgresql.Driver");
-//            driver.setUrl("jdbc:postgresql://localhost:5432/meuremediobd");
-//            driver.setUsername("postgres");
-//            driver.setPassword("root");
-//            return driver;
-//        } catch (Exception e) {
-//            throw new IllegalStateException("Erro de conexão com o banco" + e);
-//       }
-//   }
+    @Bean
+    public DataSource datasource() {
+        try {
+            DriverManagerDataSource driver = new DriverManagerDataSource();
+            driver.setDriverClassName("org.postgresql.Driver");
+            driver.setUrl("jdbc:postgresql://localhost:5432/meuremediobd");
+            driver.setUsername("postgres");
+            driver.setPassword("root");
+            return driver;
+        } catch (Exception e) {
+            throw new IllegalStateException("Erro de conexão com o banco" + e);
+       }
+   }
 
     @Bean
     public JpaVendorAdapter JpaVendorAdapter() {
