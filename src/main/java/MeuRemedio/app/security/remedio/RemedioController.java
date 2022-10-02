50f18f2d9329b44403bc92cebe5b63a7e1a1033a
Marcus <marcus.rodrigues26@hotmail.com>
2022-10-02 19:24:31 -0300

    Correções do Front: Alteração na Index.html e na tela AtualizarUsuario.html  #Receba

--- src/main/java/MeuRemedio/app/security/remedio/RemedioController.java
+++ src/main/java/MeuRemedio/app/security/remedio/RemedioController.java
@@ -82,10 +82,10 @@ public class RemedioController {
                                   @RequestParam(value = "AG_HoraInicio", required = false) String AG_horaInicio,
                                   @RequestParam(value = "AG_DataFinal", required = false)  String AG_DataFinal ,
                                   @RequestParam(value = "AG_Periodicidade", required = false) long AG_Periodicidade,
-                                  @RequestParam(value = "intervaloDias", required = false) Long intervaloDias /*,
+                                  @RequestParam(value = "intervaloDias", required = false) Long intervaloDias ,
                                   @RequestParam(defaultValue = "2022/01/01", value = "GA_Data", required = false) String GA_Data,
                                   @RequestParam(value = "GA_Valor", required = false) double GA_Valor,
-                                  @RequestParam(value = "GA_Parcela", required = false) long GA_Parcela */) throws Exception {
+                                  @RequestParam(value = "GA_Parcela", required = false) long GA_Parcela ) throws Exception {
 
         boolean auxRetiradoSUS;
 
@@ -96,9 +96,9 @@ public class RemedioController {
         Remedio remedio = new Remedio(RM_Nome, RM_Dosagem, RM_UnidadeDosagem, auxRetiradoSUS, usuarioID);
         Remedio rem = remedioRepository.save(remedio);
 
-/*        Financeiro financeiro = new Financeiro (GA_Data, GA_Valor, GA_Parcela, Collections.singletonList(rem), usuarioID.getId());
+        Financeiro financeiro = new Financeiro (GA_Data, GA_Valor, GA_Parcela, Collections.singletonList(rem), usuarioID.getId());
         controleFinanceiro.save(financeiro);
-*/
+
         if (Objects.nonNull(check) && check) {
             cadastrarAgendamento(Collections.singletonList(remedio), AG_DataInicio, AG_horaInicio, AG_DataFinal, AG_Periodicidade, intervaloDias);
         }
