e31aad7e5f8f9bf3569c8e9800462099ec97bcfc
LucasKubo <lucaskbk2@gmail.com>
2022-10-23 21:00:07 -0300

    geracao de token e envio de notificacao

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -1,11 +1,10 @@
-'use strict';
 
-import firebase from "firebase/compat";
-
-importScripts("https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.4/firebase-messaging.js");
-
-const FIREBASE_CONFIG = {
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
     apiKey: "AIzaSyBjpN-mkpDpaKOPJITaAGaYK6Y-Owv4a_c",
     authDomain: "meu-remedio-353002.firebaseapp.com",
     projectId: "meu-remedio-353002",
@@ -13,37 +12,29 @@ const FIREBASE_CONFIG = {
     messagingSenderId: "980419248726",
     appId: "1:980419248726:web:63d12fb6b805c270ad31f7",
     measurementId: "G-E7DHR6TZ0B"
-};
+});
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 
-// Initialize the firebase in the service worker.
-firebase.initializeApp(FIREBASE_CONFIG);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically
+// and you should use data messages for custom notifications.
+// For more info see:
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function(payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.notification.title;
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
     const notificationOptions = {
-        body: payload.notification.body,
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
     };
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
-self.addEventListener('notificationclick', event => {
-    console.log(event)
-});
 
-// self.addEventListener('push', function (event) {
-//     var data = event.data.json();
-//
-//     const title = data.Title;
-//     data.Data.actions = data.Actions;
-//     const options = {
-//         body: data.Message,
-//         data: data.Data
-//     };
-//     event.waitUntil(self.registration.showNotification(title, options));
-// });
-//
-// self.addEventListener('notificationclick', function (event) {});
-//
-// self.addEventListener('notificationclose', function (event) {});
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+    console.log("notificacao recebida");
+});
