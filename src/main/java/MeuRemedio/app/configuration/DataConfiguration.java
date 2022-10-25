b86460923b580cd8220ba6fe09594751c7832e06
Guilherme Fernandes Lima <44808914+FernandesGuilherme@users.noreply.github.com>
2022-10-25 17:35:45 -0300

    Update DataConfiguration.java

--- src/main/java/MeuRemedio/app/configuration/DataConfiguration.java
+++ src/main/java/MeuRemedio/app/configuration/DataConfiguration.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataConfiguration {
 
+    /*
     // Metodo para base de dados locais
     @Bean
     public DataSource datasource() {
@@ -26,6 +27,7 @@ public class DataConfiguration {
             throw new IllegalStateException("Erro de conexão com o banco" + e);
         }
     }
+    */
 
     @Bean
     public JpaVendorAdapter JpaVendorAdapter() {
