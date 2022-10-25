b688c68553d76fbef93fdcc7511d6a02d300b270
LucasKubo <lucaskbk2@gmail.com>
2022-10-24 21:20:45 -0300

    salvando token de notificacao de usuario no banco de dados

--- src/main/java/MeuRemedio/app/controllers/notificacao/notificacaoController.java
+++ src/main/java/MeuRemedio/app/controllers/notificacao/notificacaoController.java
@@ -1,5 +1,11 @@
 package MeuRemedio.app.controllers.notificacao;
 
+import MeuRemedio.app.models.usuarios.UsuarioNotificationToken;
+import MeuRemedio.app.models.usuarios.UsuarioNotificationTokenId;
+import MeuRemedio.app.repository.UsuarioNotificationTokenRepository;
+import MeuRemedio.app.service.UserSessionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class notificacaoController {
+    @Autowired
+    UsuarioNotificationTokenRepository usuarioNotificationTokenRepository;
+    @Autowired
+    UserSessionService userSessionService;
+
+
     @RequestMapping(value = "/getNotificationToken", method = RequestMethod.POST)
     public String receberTokenDeNotificacao(@RequestBody String token){
         System.out.println("Recebi o token: " + token);
+        UsuarioNotificationToken usuarioNotificationToken = new UsuarioNotificationToken();
+        UsuarioNotificationTokenId usuarioNotificationTokenId = new UsuarioNotificationTokenId();
+        usuarioNotificationTokenId.setToken(token);
+        usuarioNotificationTokenId.setUsuario(userSessionService.returnIdUsuarioLogado());
+        usuarioNotificationToken.setId(usuarioNotificationTokenId);
+        usuarioNotificationTokenRepository.save(usuarioNotificationToken);
         return "Home";
     }
 }
