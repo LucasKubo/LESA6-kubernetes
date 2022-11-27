c4199e545a6847457f3a9c311b52a2f33c690496
Any Gomes <any.gomes@outlook.com.br>
2022-11-26 23:06:16 -0300

    Ordenação Lista agendamento na home

---
+++ src/main/resources/static/script/calendarioExibir.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    const dataConst = Date.now();
+    const hoje = new Date(dataConst);
+
+    var id = document.getElementsByClassName('idAgendamento');
+    var remedios = document.getElementsByClassName('remedios');
+    var dataInicio = document.getElementsByClassName('dataInicio');
+    var dataFinal = document.getElementsByClassName('dataFinal');
+
+    var idIT = document.getElementsByClassName('idIntervaloDias');
+    var remediosIT = document.getElementsByClassName('remediosIT');
+    var dataInicioIT = document.getElementsByClassName('dataInicioIT');
+    var dataFinalIT = document.getElementsByClassName('dataFinalIT');
+    var intervaloIT = document.getElementsByClassName('intervaloDias');
+
+
+    console.log(remediosIT)
+    console.log(intervaloIT)
+    var listaAgendamentos = new Array();
+
+    for (var i = 0; i < remediosIT.length; i++) {
+        if(intervaloIT[i].innerHTML == 1){
+            break;
+        }
+        var dataFinalDate = new Date(dataFinalIT[i].innerHTML)
+        var dataFinalAjustada = new Date();
+        dataFinalAjustada.setMonth(dataFinalDate.getMonth())
+        dataFinalAjustada.setDate(dataFinalDate.getDate() + 2);
+        var dataInicioAjustada = new Date(dataInicioIT[i].innerHTML);
+        var dataInicioIntervalo = new Date();
+        dataInicioIntervalo.setMonth(dataInicioAjustada.getMonth())
+        dataInicioIntervalo.setDate(dataInicioAjustada.getDate() + 1)
+        console.log(dataInicioIntervalo.toLocaleDateString('en-CA'))
+        console.log(dataFinalAjustada.toLocaleDateString('en-CA'))
+        while (dataInicioIntervalo < dataFinalAjustada) {
+            var ag = {
+                "groupId": idIT[i].innerHTML,
+                "title": remediosIT[i].innerHTML,
+                "start": dataInicioIntervalo.toLocaleDateString('en-CA'),
+                "end": dataInicioIntervalo.toLocaleDateString('en-CA')
+            }
+            console.log(ag)
+            listaAgendamentos.push(ag);
+            dataInicioIntervalo.setDate(dataInicioIntervalo.getDate() + parseInt(intervaloIT[i].innerHTML));
+        }
+    }
+
+    for (var i = 0; i < remedios.length; i++) {
+
+        if (listaAgendamentos.filter(e => e.groupId === id[i].innerHTML).length > 0) {
+            console.log("Já adicionado")
+        } else {
+            var dataFinalDate = new Date(dataFinal[i].innerHTML)
+            var dataFinalAjustada = new Date();
+            dataFinalAjustada.setMonth(dataFinalDate.getMonth())
+            dataFinalAjustada.setDate(dataFinalDate.getDate() + 2);
+            var ag = {
+                "groupId": id[i].innerHTML,
+                "title": remedios[i].innerHTML,
+                "start": dataInicio[i].innerHTML,
+                "end": dataFinalAjustada.toLocaleDateString('en-CA')
+            }
+            listaAgendamentos.push(ag);
+        }
+    }
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['interaction', 'dayGrid'],
+        defaultDate: hoje,
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        events: listaAgendamentos
+    });
+
+    calendar.render();
+});
---
+++ src/main/resources/static/script/ordenacao.js
@@ -0,0 +1,76 @@
+function ordenar(n, num) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("data-table");
+    switching = true;
+    // Define a direção de ordenação como ascendente:
+    dir = "asc";
+    /* Faz um loop que irá continuar até que
+    nenhuma troca seja feita: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop por todas as linhas da tabela (exceto a primeira,
+      que contém headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Pega os dois elementos que serão comprados
+        a linha atual e a próxima: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check se as linhas devem trocar de lugar,
+        baseado na direção, asc or desc: */
+        if (num == true){
+            var xnumero = x.innerHTML.replace(/\D/g, "");
+            var ynumero = y.innerHTML.replace(/\D/g, "");
+            console.log(xnumero)
+            if (dir == "asc") {
+                if (Number(xnumero) > Number(ynumero)) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    console.log(shouldSwitch)
+                    break;
+                } 
+            } else if (dir == "desc") {
+                if (Number(xnumero) < Number(ynumero)) {
+                  // If so, mark as a switch and break the loop:
+                  shouldSwitch = true;
+                  console.log(shouldSwitch)
+                  break;
+                }
+              }
+        } else if (num == false){
+            console.log(dir)
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+      }
+    }
+      if (shouldSwitch) {
+        /* Se shouldSwitch = true, realiza a troca e
+        marca que a troca foi feita: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Cada vez que uma troca é realizada, aumenta a conta por 1:
+        switchcount ++;
+      } else {
+        /* Se nenhuma troca foi feita e a direção é "asc",
+        seta a direção para "desc" e roda o loop de novo. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
