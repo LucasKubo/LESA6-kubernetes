7f317cda7a7938d86e126b9641f57a34d11b2cbe
FernandesGuilherme <guilherme.logus@gmail.com>
2022-12-06 13:37:40 -0300

    Testes unitários da classe RemedioController.

--- src/main/java/MeuRemedio/app/controllers/remedio/RemedioController.java
+++ src/main/java/MeuRemedio/app/controllers/remedio/RemedioController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
--- src/test/java/MeuRemedio/app/controllers/Remedio/RemedioControllerTest.java
+++ src/test/java/MeuRemedio/app/controllers/Remedio/RemedioControllerTest.java
@@ -1,13 +1,12 @@
+
 package MeuRemedio.app.controllers.Remedio;
 
 import MeuRemedio.app.controllers.remedio.RemedioController;
 import MeuRemedio.app.mocks.AgendamentoMock;
+import MeuRemedio.app.mocks.FinanceiroMock;
 import MeuRemedio.app.mocks.RemedioMock;
 import MeuRemedio.app.models.remedios.Remedio;
-import MeuRemedio.app.repository.AgendamentoRepository;
-import MeuRemedio.app.repository.AgendamentosHorariosRepository;
-import MeuRemedio.app.repository.IntervaloDiasRepository;
-import MeuRemedio.app.repository.RemedioRepository;
+import MeuRemedio.app.repository.*;
 import MeuRemedio.app.service.CalculaHorariosNotificacao;
 import MeuRemedio.app.service.UserSessionService;
 import MeuRemedio.app.service.utils.ValidateAuthentication;
@@ -20,6 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -45,24 +45,31 @@ public class RemedioControllerTest {
     @Mock
     RemedioRepository remedioRepository;
 
-    @Mock
+    @InjectMocks
     RemedioController remedioController;
 
     @Mock
     UserSessionService userSessionService;
+    @Mock
+    FinanceiroRepository controleFinanceiro;
+
+    @Mock
+    AgendamentoRepository agendamentoRepository;
+
 
     @BeforeEach
-    public void init(){
-      //  ReflectionTestUtils.setField(remedioController, "remedioController", remedioController);
+    public void init() {
         ReflectionTestUtils.setField(remedioController, "validateAuthentication", validateAuthentication);
         ReflectionTestUtils.setField(remedioController, "userSessionService", userSessionService);
         ReflectionTestUtils.setField(remedioController, "remedioRepository", remedioRepository);
+        ReflectionTestUtils.setField(remedioController, "controleFinanceiro", controleFinanceiro);
+        ReflectionTestUtils.setField(remedioController, "agendamentoRepository", agendamentoRepository);
     }
 
 
-    @DisplayName("Deve retornar a lista de remedios")
+    @DisplayName("Deve retornar a tela de cadastro de remedios")
     @Test
-    public void verListaRemedios(){
+    public void verTelaCadastroRemedios() {
         List<Remedio> remedios = Collections.singletonList(RemedioMock.remedioMock());
 
 
@@ -71,12 +78,12 @@ public class RemedioControllerTest {
         Mockito.when(remedioRepository.findAllByUsuario(any())).thenReturn(new ArrayList<>());
 
         String rm = remedioController.telaCadastroRemedio(modelMap);
-        Assertions.assertEquals(rm, "Login");
+        Assertions.assertEquals(rm, "cadastros/CadastroRemedios");
     }
 
     @DisplayName("Deve retornar Cadastrar remedios")
     @Test
-    public void viewCadastroAgendamento(){
+    public void viewCadastroAgendamento() {
         List<Remedio> remedios = Collections.singletonList(RemedioMock.remedioMock());
 
         Mockito.when(validateAuthentication.auth()).thenReturn(true);
@@ -88,7 +95,7 @@ public class RemedioControllerTest {
 
     @DisplayName("Deve retornar falso ao deletar um remedio não cadastrado") // OK
     @Test
-    public void deletarError(){
+    public void deletarError() {
         Remedio remedio = RemedioMock.remedioMock();
         String del = remedioController.deletarRemedio(99999);
 
@@ -97,29 +104,23 @@ public class RemedioControllerTest {
 
     @DisplayName("Deve deletar um remedio")
     @Test
-    public void deletar(){
+    public void deletar() {
         Remedio remedio = RemedioMock.remedioMock();
+        Mockito.when(remedioRepository.existsById(any())).thenReturn(true);
+        Mockito.when(remedioRepository.findById(1l)).thenReturn(remedio);
+        Mockito.when(controleFinanceiro.findAllByRemedio(remedio)).thenReturn(Collections.singletonList(FinanceiroMock.gastoMock()));
+        Mockito.when(agendamentoRepository.findAllByRemedio(remedio)).thenReturn(Collections.singletonList(AgendamentoMock.agendamentoMock()));
         String del = remedioController.deletarRemedio(remedio.getRM_ID());
         Assertions.assertEquals(del, "redirect:/remedios");
-    }
-
-    @DisplayName("Deve retornar falso ao cadastrar um remedio invalido") // OK
-    @Test
-    public void CadastrarRemediosComDadosInvalidos() throws Exception {
-        List<Remedio> remedios = Collections.singletonList(RemedioMock.remedioMock());
-        String result = remedioController.CadastroRemedio("Doralgina","10","MG", "True", true,
-                "2022-01-01","00:00","2021-01-01", 24L, 2L, "25,00", 25.00,1L);
-
-        Assertions.assertFalse(Boolean.parseBoolean(result));
     }
 
     @DisplayName("Deve cadastrar um remedio ")
     @Test
     public void CadastrarRemedios() throws Exception {
-        List<Remedio> remedios = Collections.singletonList(RemedioMock.remedioMock());
-        String result = remedioController.CadastroRemedio("Doralgina","10","MG", "True", true,
-                "2022-01-01","00:00","2022-01-01", 24L, 2L, "2022-01-01", 25.00,1L);
+        String result = remedioController.CadastroRemedio("Doralgina", "10", "MG", "True", true,
+                "2022-01-01", "00:00", "2022-01-01", 24L, 2L, "2022-01-01", 25.00, 1L);
 
         Assertions.assertEquals(result, "redirect:/remedios/remedios_cadastro");
     }
 }
+
